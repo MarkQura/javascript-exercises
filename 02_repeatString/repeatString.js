acc8a8d3d12d88376928f9b5027808bbd46deb5d
MarkQura <prision09@gmail.com>
2022-07-18 21:55:59 +0100

    Exercices 01 and 02 completed
    [Ticket: X]

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let strF = ""
 
+    if (times < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < times; ++i)
+        strF = strF.concat(str);
+
+    return strF
 };
 
 // Do not edit below this line
