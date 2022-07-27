e68d2d0a414be1e313543a9cb82faf49727a8f4c
MarkQura <prision09@gmail.com>
2022-07-27 13:41:18 +0100

    Palindromes exercice complete
    function that checks if a string
    is the same when reversed.
    
    [Ticket: X]

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    str = str.toLowerCase();
+    str = Array.from(str).filter((char) => (("a" <= char && char <= "z"))).join("");
 
+    let reverse = str.split("").reverse().join("");
+    return reverse == str
 };
 
 // Do not edit below this line
