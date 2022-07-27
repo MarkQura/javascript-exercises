41a887336053306599afab5ec7d91be880a37f3c
MarkQura <prision09@gmail.com>
2022-07-27 13:51:31 +0100

    Fibonnaci exercice complete
    functon that calculates the fibonacci sequence up to an index
    
    [Ticket: X]

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (typeof(index) != "number") index = +index;
+    if (index < 0) return "OOPS";
 
+    let prev = 0;
+    let current = 1;
+    let temp;
+
+    for (let i = 1; i < index; ++i) {
+        temp = current;
+        current += prev;
+        prev = temp;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
