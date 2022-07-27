46e16bca99fcf64d8b5d22783809beb615ebbf57
MarkQura <prision09@gmail.com>
2022-07-27 14:30:39 +0100

    Finished findTheOldest exercise
    sorted the array of objs to have
    the oldest as the first element
    then returns it
    
    [Ticket: X]

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let result = arr.sort((first, second) => {
+        let date = new Date().getFullYear();
+        let age1 = (first.yearOfDeath == null ? date :  first.yearOfDeath) - first.yearOfBirth;
+        let age2 = (second.yearOfDeath == null ? date :  second.yearOfDeath) - second.yearOfBirth;
+        return age1 < age2 ? 1 : -1;
+    });
 
+    return result[0];
 };
 
 // Do not edit below this line
